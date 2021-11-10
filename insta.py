1813a09afb8f7d0ad9fad37af1e32f5646816867
Artur Karpovich <onelifeitsme@gmail.com>
2021-11-10 08:21:57 +0300

    done

--- insta.py
+++ insta.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from time import sleep
 import csv
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+
 
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36', 'accept': '*/*'}
 mobile_emulation = { "deviceName": "Nexus 5" }
@@ -19,19 +18,15 @@ password = 'OneLife2019'
 
 driver = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
 
-
-
 sleep(1)
 
 # ФУНКЦИЯ АВТОРИЗАЦИИ
 def sign_ing(login, password):
-    login = 'onelifeitsme'
-    pasword = 'OneLife2019'
     sleep(3)
     login_field = driver.find_element(By.XPATH,'//div[3]/div/label/input')
     login_field.send_keys(login)
     password_field = driver.find_element(By.XPATH,'//div[4]/div/label/input')
-    password_field.send_keys(pasword)
+    password_field.send_keys(password)
     driver.find_element(By.XPATH,'//*[@id="loginForm"]/div[1]/div[6]/button/div').click()
     sleep(4)
 
@@ -72,102 +67,105 @@ users = driver.find_elements(By.XPATH, '//li/div/div[1]/div[2]/div[1]/a')
 print('Получено ', len(users), ' аккаунтов')
 accounts = ['https://www.instagram.com/' + user.text for user in users]
 
-# base = []
-#
-#
-# def parse_account_info(account):
-#     try:
-#         close_or_not = driver.find_element('//div[1]/div/h2').text
-#     except:
-#         close_or_not = 'открытый'
-#     if "закрытый аккаунт" in close_or_not:
-#         try:
-#             name = driver.find_element('//div[2]/h1').text
-#         except:
-#             name = ' '
-#         post_number = driver.find_element('//li[1]/span/span').text
-#         followers_number = driver.find_element('//li[2]/span/span').text
-#         following_number = driver.find_element('//li[3]/span/span').text
-#         try:
-#             bio = driver.find_element('//div[2]/span').text
-#         except:
-#             bio = ' '
-#         try:
-#             link = driver.find_element('//div/div[1]/a').text
-#             if 'подписан' in link:
-#                 link = ' '
-#         except:
-#             link = ' '
-#         category = ' '
-#
-#
-#     else:
-#         try:
-#             name = driver.find_element('//div[2]/h1').text
-#         except:
-#             name = ' '
-#         try:
-#             post_number = driver.find_element('//li[1]/span/span').text
-#         except:
-#             post_number = ' '
-#         try:
-#             followers_number = driver.find_element('//li[2]/a/span').text
-#         except:
-#             followers_number = ' '
-#         try:
-#             following_number = driver.find_element('//li[3]/a/span').text
-#         except:
-#             following_number = ' '
-#         try:
-#             category = driver.find_element('//header/section/div[2]/div/span').text
-#         except:
-#             category = ' '
-#         try:
-#             bio = driver.find_element('//div[2]/span').text
-#         except:
-#             bio = ' '
-#         try:
-#             link = driver.find_element('//header/section/div[2]/a').text
-#             if 'подписан' in link:
-#                 link = ' '
-#         except:
-#             link = ' '
-#
-#     base.append({
-#         'Аккаунт': account,
-#         'Имя': name,
-#         'Категория': category,
-#         'Постов': post_number,
-#         'Подписчиков': followers_number,
-#         'Подписок': following_number,
-#         'БИО': bio,
-#         'Ссылка в био': link,
-#
-#     })
-#
-#
-# account_number = 0
-# for i in accounts:
-#     try:
-#         driver.get(accounts[account_number])
-#     except:
-#         pass
-#     sleep(2)
-#     try:
-#         parse_account_info(accounts[account_number])
-#     except:
-#         pass
-#     sleep(2)
-#     account_number +=1
-#     print(account_number)
-#
-# # СОЗДАНИЕ ФАЙЛА И ЗАПИСЬ К НЕГО ИТОГА ПАРСИНГА
-# with open("insta.csv", "w", encoding='utf-8') as temp:
-#     polya = ['Аккаунт', 'Имя', 'Категория', 'Постов', 'Подписчиков', 'Подписок', 'БИО', 'Ссылка в био']
-#     out = csv.DictWriter(temp, fieldnames=polya)
-#     out.writeheader()
-#     for i in base:
-#         out.writerow(i)
+base = []
+
+# ФУНКЦИЯ ПАРСИНГА ИНФЫ ВСЕХ ПОЛЕЙ В АККАУНТЕ
+def parse_account_info(account):
+    try:
+        close_or_not = driver.find_element(By.XPATH,'//div[1]/div/h2').text
+    except:
+        close_or_not = 'открытый'
+    if "закрытый аккаунт" in close_or_not:
+        try:
+            name = driver.find_element(By.XPATH,'//main/div/div[1]/h1').text
+        except:
+            name = ' '
+        post_number = driver.find_element(By.XPATH, '//main/div/ul/li[1]/span/span').text
+        followers_number = driver.find_element(By.XPATH, '//main/div/ul/li[2]/a/span').text
+        following_number = driver.find_element(By.XPATH, '//main/div/ul/li[3]/a/span').text
+        try:
+            bio = driver.find_element(By.XPATH, '//main/div/div[1]/span').text
+        except:
+            bio = ' '
+        try:
+            link = driver.find_element(By.XPATH, '//main/div/div[1]/a').text
+            if 'подписан' in link or 'follow' in link:
+                link = ' '
+        except:
+            link = ' '
+        category = ' '
+
+
+    else:
+        try:
+            name = driver.find_element(By.XPATH, '//main/div/div[1]/h1').text
+        except:
+            name = ' '
+        try:
+            post_number = driver.find_element(By.XPATH, '//li[1]/span/span').text
+        except:
+            post_number = ' '
+        try:
+            followers_number = driver.find_element(By.XPATH, '//li[2]/a/span').text
+        except:
+            followers_number = ' '
+        try:
+            following_number = driver.find_element(By.XPATH, '//li[3]/a/span').text
+        except:
+            following_number = ' '
+        try:
+            category = driver.find_element(By.XPATH, '//main/div/div[1]/div/span').text
+        except:
+            category = ' '
+        try:
+            bio = driver.find_element(By.XPATH, '//main/div/div[1]/span').text
+        except:
+            bio = ' '
+        try:
+            link = driver.find_element(By.XPATH, '//main/div/div[1]/a').text
+            if 'подписан' in link or 'follow' in link:
+                link = ' '
+        except:
+            link = ' '
+
+    base.append({
+        'Аккаунт': account,
+        'Имя': name,
+        'Категория': category,
+        'Постов': post_number,
+        'Подписчиков': followers_number,
+        'Подписок': following_number,
+        'БИО': bio,
+        'Ссылка в био': link,
+
+    })
+
+
+# ПРОХОД ПО КАЖДОМУ АККАУНТУ, ПАРСИНГ И ЗАПИСЬ ИНФЫ В СПИСОК СЛОВАРЕЙ base
+accounts = ['https://www.instagram.com/zorachka_agency/']
+account_number = 0
+for acc in accounts:
+    try:
+        driver.get(accounts[account_number])
+    except:
+        pass
+    sleep(2)
+    try:
+        parse_account_info(accounts[account_number])
+    except:
+        pass
+    sleep(2)
+    account_number +=1
+    print(account_number)
+
+
+# СОЗДАНИЕ ФАЙЛА И ЗАПИСЬ К НЕГО ИТОГА ПАРСИНГА
+with open("insta.csv", "w", encoding='utf-8') as temp:
+    polya = ['Аккаунт', 'Имя', 'Категория', 'Постов', 'Подписчиков', 'Подписок', 'БИО', 'Ссылка в био']
+    out = csv.DictWriter(temp, fieldnames=polya)
+    out.writeheader()
+    for i in base:
+        out.writerow(i)
 
 
 
